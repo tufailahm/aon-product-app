1d99f6e38dd373fb7112555be3435ad8c9b2a75d
Tufail Ahmed <tufailahmedkhan@gmail.com>
2023-06-22 14:49:46 +0530

    first commit

--- src/app/components/addproduct/addproduct.component.ts
+++ src/app/components/addproduct/addproduct.component.ts
@@ -4,7 +4,11 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-addproduct',
   templateUrl: './addproduct.component.html',
-  styleUrls: ['./addproduct.component.css']
+  styles: [` 
+  input.ng-invalid { border-left:5px solid red; }
+  input.ng-valid { border-left: 5px solid green; }
+  `]
+
 })
 export class AddproductComponent {
   productForm = new FormGroup({
